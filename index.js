7a09a2c125d0d7ea50dd80448175dc745c95dc61
AnaIordan183 <iordanana1803@gmail.com>
2025-02-11 12:49:47 +0200

    index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const dog = document.querySelector(".dog img");
+
+    dog.addEventListener("click", () => {
+        alert("Happy Valentine's Day! ❤️🐶🍫");
+    });
+});
